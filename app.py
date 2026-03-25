80101670b73ef5510457c975856f88eaeb1078d7
MunimAkbar <munimakbar113@gmail.com>
2026-03-25 21:31:31 +0500

    fix: app run debug set to False

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def predictRoute():
 if __name__ == "__main__":
     host = os.environ.get("FLASK_RUN_HOST", "127.0.0.1")
     port = int(os.environ.get("FLASK_RUN_PORT", 8080))
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port, debug=False)
